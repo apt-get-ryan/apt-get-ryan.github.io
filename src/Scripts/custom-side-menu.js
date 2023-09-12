70bbd67fb7c38bb5e50272a26ba04a9cf405e6cb
Ryan Guimarães <ryan.guimar10@gmail.com>
2023-09-12 01:22:57 -0300

    Otimização do menu lateral, correção no rodapé de design, ajuste no css do bootstrap. remoção de regras de css que foram esquecidas de serem removidas na última verificação

--- src/Scripts/custom-side-menu.js
+++ src/Scripts/custom-side-menu.js
@@ -1,43 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
     const contentWrapper = window['all-contents-wrapper'];
     window['sidemenu-container'].addEventListener('show.bs.offcanvas', () => {
-        var scrollPosition = window.scrollY;
-        window['all-contents-wrapper'].classList.remove("expand-contents");
+        var scrollPosition = window.scrollY === 0 ? document.querySelector("main").scrollTop : window.scrollY;
+        contentWrapper.classList.remove("shrinkfy-contents");
         contentWrapper.classList.add("shrinkfy-contents");
-        document.querySelector("main").scroll({
-            top: scrollPosition,
-            behavior: 'smooth'
-          });
-    })
-
-    window['sidemenu-container'].addEventListener('hide.bs.offcanvas', () => {
-        var scrollPosition = document.querySelector("main").scrollTop;
-
         const animationTiming = {
-            duration: 400,             
-            iterations: 1, 
+            duration: 500,
+            iterations: 1,
             easing: 'ease',
+            fill: 'forwards'
         };
         const animationKeyframes = [
             {
-                transform: 'scale(0.7) translateX(calc((-1 * 400px * 1.2)))',
-                maxHeight: '100vh',
-                overflowY: 'scroll'
-            },
-            {
-                transform: 'scale(1)',
-                maxHeight: '100vh',
-                overflowY: 'scroll'
-            },
-        ];
-        contentWrapper.animate(animationKeyframes, animationTiming)
-            .onfinish = function (e) {
-                window.scroll({
-                    top: scrollPosition,
-                    behavior: 'smooth'
-                })
-                
+                transform: 'scale(0.7) translateX(-480px)'
             }
+        ];
+        contentWrapper.style.maxHeight = '100vh';        
+        document.querySelector("main").scroll({
+            top: scrollPosition
+        });
+        contentWrapper.animate(animationKeyframes, animationTiming);
+    })
+
+    window['sidemenu-container'].addEventListener('hide.bs.offcanvas', () => {
+        let scrollPosition = document.querySelector("main").scrollTop;
+        const animationTiming = {
+            duration: 500,
+            iterations: 1,
+            easing: 'ease',
+            fill: 'forwards'
+        };
+        const animationKeyframes = [
+            {
+                transform: 'scale(1) translateX(0px)',
+            }
+        ];
         contentWrapper.classList.remove("shrinkfy-contents");
+        contentWrapper.style.paddingRight = '10px';
+        contentWrapper.animate(animationKeyframes, animationTiming).onfinish = (() => {
+            contentWrapper.style.maxHeight = '';
+            contentWrapper.style.paddingRight = '0px';
+            window.scroll({
+                top: scrollPosition
+            })
+        });
     })
 })
