9b80872216bf800725b0a5132f072f476d2a27ed
Ryan Guimarães <ryan.guimar10@gmail.com>
2025-08-20 02:19:58 -0300

    corrigido link do header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,10 @@
 "use client"
 import Image from "next/image";
 
-import HeaderButton from "./HeaderButton";
+import HeaderButton from "./HeaderButton.js";
 import { mergeClassNames } from "@/utils/common";
 import { useMenuStore } from "@/stores/menu";
-
+import Link from "next/link";
 
 const Header = () => {
   const { isOpen } = useMenuStore();
@@ -13,9 +13,9 @@ const Header = () => {
   return (
     <header className={mergeClassNames("p-4 shadow-lg shadow-black/5 w-full sticky top-0 z-10 bg-[#e7e7e7]", isOpen && "rounded-t-2xl")}>
         <div className="mx-auto px-6 flex items-center justify-between w-full container max-w-screen-xl">
-            <a href="/">
+            <Link href="/">
               <Image src="/logo.svg" alt="Home" height={50} width={50}/>
-            </a>
+            </Link>
             <div>
               <HeaderButton/>
             </div>
