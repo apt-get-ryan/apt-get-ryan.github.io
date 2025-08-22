74f36c77a68b456b7906a207031d14d8e91e6745
Ryan Guimarães <ryan.guimar10@gmail.com>
2025-08-22 00:08:32 -0300

    Adicionado focus-trap ao menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -5,10 +5,10 @@ import { mergeClassNames } from '@/utils/common';
 import { useEffect, useRef } from 'react';
 import { FaCode, FaPalette, FaUserGear } from 'react-icons/fa6';
 import { usePathname } from 'next/navigation';
-
+import { FocusTrap } from 'focus-trap-react';
 function Overlay({onClose, isOpen}) {
     return (
-        <div className={mergeClassNames('h-full w-full absolute z-10 hidden', isOpen && "!block")} onClick={onClose}></div>
+        <div role="presentation" aria-hidden={!isOpen} className={mergeClassNames('h-full w-full absolute z-10 hidden', isOpen && "!block")} onClick={onClose}></div>
     )
 }
  
@@ -37,27 +37,31 @@ function Menu() {
     }, [isOpen])
     return (
         <>
-            <nav tabIndex="-1" ref={navRef} className={mergeClassNames("bg-slate-100 fixed outline-none h-dvh w-[400px] right-0 p-4 transition-transform translate-x-full z-20 duration-500", isOpen && "!translate-x-0",
-                "grid grid-rows-[auto_auto_1fr_auto_auto]"
-            )}>
-                <div className="flex justify-end items-center">
-                    <MenuCloseButton/>
-                </div>
-                <hr className='h-px my-4 border-gray-400/75'/>
-                <div className='overflow-auto'>
-                    <ul className=''>
-                        {menuItems.map((menuItem, itemIndex) => 
-                            <li key={itemIndex} className={`py-2 px-4 text-opacity-70 hover:text-opacity-100 text-slate-950 text-lg ${ pathName === menuItem.href && " text-opacity-100 pointer-events-none !text-blue-600"}`}>
-                                <a href={menuItem.href} className="flex gap-2 items-center">
-                                    {menuItem.icon} {menuItem.label}
-                                </a>
-                            </li>
-                        )}
-                    </ul>
+          <FocusTrap active={isOpen}>
+            <div>
+              <nav tabIndex="-1" ref={navRef} className={mergeClassNames("bg-slate-100 fixed outline-none h-dvh w-full max-w-[400px] right-0 p-4 transition-transform translate-x-full z-20 duration-500", isOpen && "!translate-x-0",
+                  "grid grid-rows-[auto_auto_1fr_auto_auto]"
+              )}>
+                  <div className="flex justify-end items-center">
+                      <MenuCloseButton/>
+                  </div>
+                  <hr className='h-px my-4 border-gray-400/75'/>
+                  <div className='overflow-auto'>
+                      <ul className=''>
+                          {menuItems.map((menuItem, itemIndex) => 
+                              <li key={itemIndex} className={`py-2 px-4 text-opacity-70 hover:text-opacity-100 text-slate-950 text-lg ${ pathName === menuItem.href && " text-opacity-100 pointer-events-none !text-blue-600"}`}>
+                                  <a href={menuItem.href} className="flex gap-2 items-center">
+                                      {menuItem.icon} {menuItem.label}
+                                  </a>
+                              </li>
+                          )}
+                      </ul>
 
-                </div>
-            </nav>
-            <Overlay onClose={close} isOpen={isOpen}/>
+                  </div>
+              </nav>
+              <Overlay onClose={close} isOpen={isOpen}/>
+            </div>
+          </FocusTrap>
         </>
     )
 }
